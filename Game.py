4b1b06cc1d7e0b0ee3947e1ca7e8941788675fbc
Blancaslan <68610191+Blancaslan@users.noreply.github.com>
2022-05-13 10:07:35 +0100

    Can change what object is called

--- Game.py
+++ Game.py
@@ -1,47 +1,52 @@
+# Possible options for user input referanced in game loop
 building_types = ["house", "office", "factory"]
 positions = [1, 2, 3, 4]
 narcos = ["exit", "build", "destroy", "check"]
-class land():
-    pass
+buildings = []
 
+# Class used to create building objects and alter them with methods
 class plot: 
 
-    def __init__(self, position, building_type, quality, size, population, land_value):
+    def __init__(self, position, building_type, population, land_value):
         self.position = position
         self.building_type = building_type
-        self.quality = quality
-        self.size = size
         self.population = population
         self.land_value = land_value
         
     def Get_attributes(self):
-        return self.position, self.building_type, self.quality, self.size, self.population, self.land_value
+        return self.position, self.building_type, self.population, self.land_value
 
-    def Set_attributes(self, position, building_type):
-        if (position in positions):
-            self.position = position
+    def Set_attributes(self, building_type):
         if (building_type in building_types):
             self.building_type = building_type
 
     def Destroy_attributes(self):
         self.position = 0
         self.building_type = ""
-        self.quality = 0
-        self.size = 0
         self.population = 0
         self.land_value = 0
-House1 = plot(0, "", 0, 0, 0, 0)
+    
+    def Set_land_value(self):
+        self.land_value = self.population * self.building_type
 
+# Building objects
+Building1 = plot(0, "", 0, 0)
+Building2 = plot(1, "", 0, 0)
+buildings.append(Building1)
+buildings.append(Building2)
+
+# Game loop used to alter building objects and exit game
 while(True):
+    buildin = int(input("what building number: "))
     narco = str(input("What do you want to do: "))
     if narco in narcos:
         if narco == "exit":
             exit()
         elif narco == "build":
-            House1.Set_attributes(int(input("What location: ")), str(input("What building type: ")))
+            buildings[buildin - 1].Set_attributes(str(input("What building type: ")), )
         elif narco == "check":
-            print(House1.Get_attributes())
+            print(buildings[buildin - 1].Get_attributes())
         elif narco == "destroy":
-            House1.Destroy_attributes()
+            buildings[buildin - 1].Destroy_attributes()
     else:
         pass
